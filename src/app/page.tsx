4200c13e5bd7b685c79f052ff3a48935333d84c5
Vishruth Raj <vishraj248@gmail.com>
2023-06-13 22:24:09 -0700

    Add loading screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
 import NavBar from "./components/NavBar";
 import Landing from "./components/Landing";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import { Analytics } from "@vercel/analytics/react";
+import { useHydrated } from "@/hooks/useHydrated";
 
 export default function Home() {
+  const hydrated = useHydrated();
+
+  if (!hydrated) {
+    return (
+      <div className="flex items-center justify-center bg-backgroundBlack w-full h-full min-h-screen">
+        <img src="/logo.png" alt="Vishruth Raj logo" />
+      </div>
+    );
+  }
+
   return (
     <div className="bg-backgroundBlack w-full h-full min-h-screen">
       <NavBar />
