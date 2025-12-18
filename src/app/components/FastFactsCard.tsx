f783894734826ae20138ce81d80db87c94d6927e
Vishruth Raj <vishraj248@gmail.com>
2025-12-18 14:39:13 -0600

    Adjust font of facts card

--- src/app/components/FastFactsCard.tsx
+++ src/app/components/FastFactsCard.tsx
@@ -28,15 +28,15 @@ const FastFactsCard = () => {
       >
         <div className="relative border-2 border-brownMuted rounded-2xl h-[150px] md:w-[75%] sm:w-[90%] px-2 flex flex-col items-center bg-background text-center select-none">
           <div className="flex-1 flex items-center justify-center">
-            <p className="font-playfair md:text-lg sm:text-sm text-brownMuted leading-relaxed">
+            <p className="font-inter md:text-lg sm:text-sm text-brownMuted leading-relaxed">
               {facts[index]}
             </p>
           </div>
           <div className="w-full flex mb-2 items-center justify-center px-4 relative">
-            <div className="absolute left-2 text-xs text-brownMuted opacity-60 font-sans">
+            <div className="absolute left-2 text-xs text-brownMuted opacity-60 font-inter">
               {index + 1}/{facts.length}
             </div>
-            <div className="gap-2 flex items-center text-brownDark opacity-60 text-sm font-sans">
+            <div className="gap-2 flex items-center text-brownDark opacity-60 text-sm font-inter">
               <span>Click to see more</span>
               <BsMouseFill />
             </div>
